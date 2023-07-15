c787559c75333d4ed61bbce778f974c001d7ef1e
AnasB97 <anas.badi97@gmail.com>
2023-07-14 18:19:13 -0700

    3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,18 +6,19 @@
  * Return: Always 0 (Success)
  */
 
-int main() {
-    char lowercase;
-    char uppercase;
-
-    printf("Lowercase and Uppercase alphabets:\n");
-    for(lowercase = 'a'; lowercase <= 'z'; lowercase++) {
-        putchar(lowercase);
-    }
-    for(uppercase = 'A'; uppercase <= 'Z'; uppercase++) {
-        putchar(uppercase);
-    }
-
-    return 0;
+int main(void)
+{
+char lowercase;
+char uppercase;
+printf("Lowercase and Uppercase alphabets:\n");
+for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
+{
+putchar(lowercase);
+}
+for (uppercase = 'A'; uppercase <= 'Z'; uppercase++)
+{
+putchar(uppercase);
+}
+return (0);
 }
 
